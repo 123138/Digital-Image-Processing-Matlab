9f60de40cb8e4518d7909175c9317d70eeff248c
Emine Durmuşkaya <61375892+123138@users.noreply.github.com>
2022-05-02 19:29:48 +0300

    Convolution Function

---
+++ Convolution2D_ED.m
@@ -0,0 +1,42 @@
+%Created on Wed 2022.03.19 00:07
+%Author Emine DURMUSKAYA
+%Last updated on 2022.04.01 19.06
+%Song # Black Sabbath - Paranoid
+
+function filteredImage = Convolution2D_ED(Image, Filter)
+
+    [M,N,RGB]=size(Image);
+    if length(RGB)==3   %transform image to black and white image
+        Image = rgb2gray(Image);
+    end
+    
+    Image = double(Image); %turning image into double in order to do mathematical operations
+    [filterRow, filterColumn] = size(Filter);  %take size of filter for offsets
+    [imageRow, imageColumn] = size(Image);
+    filteredImage(1:imageRow,1:imageColumn)=Image(1:imageRow,1:imageColumn); %create an image equal to original image, 
+    %this image will be updated after each operation
+    
+    %offsets
+    upperOffset = round(filterRow/2);   %rounds up for 5x5 filter result 3   
+    bottomOffset = floor(filterRow/2);  %for 5x5 filter result 2
+    leftOffset = round(filterColumn/2);      
+    rightOffset = floor(filterColumn/2);   
+    
+    for instantRow = upperOffset:imageRow-bottomOffset
+        for instantColumn = leftOffset:imageColumn-rightOffset
+            %create an instant matrix with center of specified pixel, that will be multiplied by filter
+            instantMatrix = Image(instantRow-bottomOffset:instantRow+bottomOffset, instantColumn-rightOffset:instantColumn+rightOffset);
+            result = 0; 
+            for instantFilterRow = 1: filterRow
+                for instantFilterColumn = 1: filterColumn
+                    %obtaining the result for center pixel
+                    result = result + Filter(instantFilterRow, instantFilterColumn)*instantMatrix(instantFilterRow,instantFilterColumn);
+                end
+            end
+            %updating the filtered image
+            filteredImage(instantRow,instantColumn)=result;
+        end
+    end
+    %converting filtered image double to uint8
+    filteredImage = uint8(filteredImage);
+end
